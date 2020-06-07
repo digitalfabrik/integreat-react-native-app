d5d0daa76b949cd9abf7a2e944f12610c92ef03d
Steffen Kleinle <klinzo@yahoo.de>
2020-06-07 10:04:44 +0200

    NATIVE-532: Fix flow

--- flow-typed/globals.js
+++ flow-typed/globals.js
@@ -1,2 +1,5 @@
+import type { BuildConfigType } from '../build-configs/configs/BuildConfigType'
+
 declare var __DEV__: boolean
 declare var __VERSION__: string
+declare var __CONFIG__: BuildConfigType
