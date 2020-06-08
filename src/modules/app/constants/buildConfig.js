1f075fbb2c8499cac87884da7a6337db316f4d27
Steffen Kleinle <klinzo@yahoo.de>
2020-06-08 11:30:37 +0200

    NATIVE-532: Simplify build config

--- src/modules/app/constants/buildConfig.js
+++ src/modules/app/constants/buildConfig.js
@@ -1,29 +1,22 @@
 // @flow
 
 import type { BuildConfigType } from '../../../../build-configs/configs/BuildConfigType'
+import integreatBuildConfig from '../../../../build-configs/configs/integreat'
+import integreatTestCmsBuildConfig from '../../../../build-configs/configs/integreat-test-cms'
+import malteBuildConfig from '../../../../build-configs/configs/malte'
 
-let config: ?BuildConfigType = null
-
-const requireConfig = (): BuildConfigType => {
-  const validConfigNames = ['integreat', 'integreat-test-cms', 'malte']
-  const buildConfigName = __CONFIG_NAME__
-
-  if (!buildConfigName) {
-    throw new Error('You need to specify a config name!')
-  } else if (!validConfigNames.includes(buildConfigName)) {
-    throw new Error(`Invalid config name! Allowed configs: ${validConfigNames.join()}`)
-  }
-  // $FlowFixMe Ignore 'The parameter passed to `require()` must be a string literal'
-  return require(`../../../../build-configs/configs/${__CONFIG_NAME__}`)
+const buildConfigs = {
+  integreat: integreatBuildConfig,
+  'integreat-test-cms': integreatTestCmsBuildConfig,
+  malte: malteBuildConfig
 }
 
 const buildConfig = (): BuildConfigType => {
-  if (!config) {
-    config = requireConfig()
-    console.warn(config)
-    return config
+  const buildConfig: ?BuildConfigType = buildConfigs[__CONFIG_NAME__]
+  if (!buildConfig) {
+    throw Error('Invalid __CONFIG_NAME__ supplied.')
   }
-  return config
+  return buildConfig
 }
 
 export default buildConfig
